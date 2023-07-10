d630cdbe994e9e4403c075ddeb3a2b5611437a1a
yoken1021 <yoken1021@gmail.com>
2023-07-11 02:30:05 +0800

    debug18

--- app/handlers/talk.js
+++ app/handlers/talk.js
@@ -29,7 +29,7 @@ const exec = (context) => check(context) && (
     let sensitive_words = config.SENSITIVE_WORDS.split(',');
     let sensitive_bool = false;
     for (let i = 0 ; i < sensitive_words.length ; i++){
-      if (context.event.text?.toString?.includes(sensitive_words[i]) || context.transcription?.toString?.includes(sensitive_words[i])){
+      if (context.event.text?.includes(sensitive_words[i]) || context.transcription?.includes(sensitive_words[i])){
         sensitive_bool = true;
       }
     }
